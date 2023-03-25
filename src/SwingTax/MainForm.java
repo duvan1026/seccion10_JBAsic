66456a26d46b85d4191793b0a6a9b022313770c4
Duvan Castro <duvancastro1026@gmail.com>
2023-03-25 06:35:11 -0500

    implement event four calcule IVA

--- src/SwingTax/MainForm.java
+++ src/SwingTax/MainForm.java
@@ -1,6 +1,7 @@
 package SwingTax;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
@@ -22,17 +23,28 @@ public class MainForm {
 
     public MainForm(){
 
-     //   buttonCalculate.addActionListener(botonPulsado());
-
-        pricePane.addKeyListener(new KeyAdapter() {
+        buttonCalculate.addActionListener(new ActionListener() {
             @Override
-            public void keyTyped(KeyEvent e) {
-                super.keyTyped(e);
-                String valorPrecio = pricePane.getText() + "Ñ";
-                pricePane.setText(valorPrecio);
+            public void actionPerformed(ActionEvent e) {
+                double p = Double.parseDouble(pricePane.getText());
+                double t = Double.parseDouble(taxPane.getText());
+
+                double result = p + ( p/ 108 * t );
+                totalPane.setText(Double.toString(result));
 
             }
         });
+
+
+//        pricePane.addKeyListener(new KeyAdapter() {
+//            @Override
+//            public void keyTyped(KeyEvent e) {
+//                super.keyTyped(e);
+//                String valorPrecio = pricePane.getText() + "Ñ";
+//                pricePane.setText(valorPrecio);
+//
+//            }
+//        });
     }
 
 
