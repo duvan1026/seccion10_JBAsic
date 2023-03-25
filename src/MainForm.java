8a6a31c0c73a92059bea46b1bcc059f4a03a920c
Duvan Castro <duvancastro1026@gmail.com>
2023-03-25 05:24:57 -0500

    create first interface of users

--- src/MainForm.java
+++ src/MainForm.java
@@ -1,2 +1,8 @@
+import javax.swing.*;
+
 public class MainForm {
+    private JTextPane pricePane;
+    private JTextPane taxPane;
+    private JTextPane totalPane;
+    private JButton buttonCalculate;
 }
